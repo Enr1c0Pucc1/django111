82f42fceab6d09af3e9714ab63deaca1cd579170
Pucci <greenymanxcreative@gmail.com>
2024-03-08 17:05:37 +0300

    Add index page layout
    - Add index page layout
    - use static template tag

--- blog-backend-master/sensive_blog/urls.py
+++ blog-backend-master/sensive_blog/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
